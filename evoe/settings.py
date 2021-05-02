9ffd8ec394e516d855270adba80a2a0bac567742
Leonardo <mendes.lnr@gmail.com>
2021-05-02 19:39:34 -0300

    deploy

--- evoe/settings.py
+++ evoe/settings.py
@@ -69,11 +69,11 @@ DATABASES = {
     # }
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'evoe',
-        'USER': os.environ.get('EVOE_DB_USER'),
-        'PASSWORD': os.environ.get('EVOE_DB_PASS'),
-        'HOST': os.environ.get('EVOE_DB_HOST'),
-        'PORT': 5432,
+        'NAME': os.environ.get('RDS_DB_NAME'),
+        'USER': os.environ.get('RDS_USERNAME'),
+        'PASSWORD': os.environ.get('RDS_PASSWORD'),
+        'HOST': os.environ.get('RDS_HOSTNAME'),
+        'PORT': os.environ.get('RDS_PORT'),
     }
 }
 
