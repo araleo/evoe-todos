dd78aa0bec1b71e1ee5c7639392d386d3b4265e7
Leonardo <mendes.lnr@gmail.com>
2021-05-02 18:46:41 -0300

    deploy

--- todo/models.py
+++ todo/models.py
@@ -4,13 +4,14 @@ from django.db import models
 
 class ToDo(models.Model):
     todo = models.CharField(max_length=100)
-    description = models.CharField(max_length=300, null=True, blank=True)
+    description = models.CharField(max_length=250, null=True, blank=True)
     important = models.BooleanField(default=False)
     user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='todos')
     created_at = models.DateTimeField(auto_now_add=True)
     last_modified_at = models.DateTimeField(auto_now=True)
     completed_at = models.DateTimeField(null=True, blank=True)
     deleted_at = models.DateTimeField(null=True, blank=True)
+
 
     def __str__(self):
         return f'{self.todo} de {self.user}'
